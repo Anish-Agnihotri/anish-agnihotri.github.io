a13dad762d43ce2d7598868a00f23c9c0ef9a971
Anish-Agnihotri <contact@anishagnihotri.com>
2018-12-12 20:33:12 -0500

    optimization:

--- assets/scripts/carousel.js
+++
@@ -1,9 +0,0 @@
-$(".scroller").owlCarousel({
-    items: 3,
-    itemsDesktop: [881, 3],
-    itemsTablet: [880, 2],
-    itemsTabletSmall: [590, 1],
-    autoPlay: false,
-    responsive: true,
-    navigation: false,
-});
